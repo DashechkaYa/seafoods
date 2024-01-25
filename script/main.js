2de18ecaf9b8a36822e48a08f87ce65971889b71
Daria Pashchenko <kyivdaria@gmail.com>
2024-01-25 13:59:04 +0200

    start slider

--- script/main.js
+++ script/main.js
@@ -1,1 +1,51 @@
 "use strict";
+
+const heroSlides = [
+{
+  title: 'Die Revolution der Gewürze',
+  description: 'Jeden Tag eine gute Tat. Mit unserem direkten Gewürzhandel kommt mehr Geld bei den Erzeuger*innen im Nahen Osten an und soziale Projekte in der Region erhalten 20% vom Umsatz. Zusammen machen wir den Unterschied!',
+  color: '#363636',
+  colorButton: '#3F3F3F',
+  image: '',
+  option: [],
+},
+{
+  title: 'KORIANDER',
+  description: 'Die blumige, frische Koriandersaat ist geschmacklich nicht mit dem Koriandergrün zu vergleichen. Perfekt um jedes deiner Pfannengerichte abzurunden.',
+  color: '#E9CDB1',
+  colorButton: '#DDC1A0',
+  image: '',
+  option: [
+    {name: 'HERKUNFT', value: 'SYRIEN'},
+    {name: 'REGION', value: 'NAHER OSTEN'},
+    {name: 'VERARBEITUNG', value: 'GESCHROTET'},
+    {name: 'GESCHMACK', value: 'FRUCHTIG'},
+  ],
+},
+{
+  title: 'KREUZ KÜMMEL',
+  description: 'Die frische Schärfe des Kreuzkümmels ist fester Bestandteil der orientalischen Küche. Ob im Hummus oder Falafel, Kreuzkümmel ist eine Geschmacksreise in die Tiefe des Orients.',
+  color: '#E9B79D',
+  colorButton: '#E0AB90',
+  image: '',
+  option: [
+    {name: 'HERKUNFT', value: 'SYRIEN'},
+    {name: 'REGION', value: 'NAHER OSTEN'},
+    {name: 'VERARBEITUNG', value: 'GESCHROTET'},
+    {name: 'GESCHMACK', value: 'FRUCHTIG'},
+  ],
+},
+{
+  title: 'PAPRIKA',
+  description: 'Unser fruchtig warmer Paprika bringt Sonne in deine Küche. Dieses Gewürz rundet viele Gerichte ab. Im Gulasch oder im Dip - Paprika bringt dir die nötige Wärme.',
+  color: '#BA6969',
+  colorButton: '#AE5E5E',
+  image: '',
+  option: [
+    {name: 'HERKUNFT', value: 'SYRIEN'},
+    {name: 'REGION', value: 'NAHER OSTEN'},
+    {name: 'VERARBEITUNG', value: 'PULVER'},
+    {name: 'GESCHMACK', value: 'FRUCHTIG'},
+  ],
+}
+];
